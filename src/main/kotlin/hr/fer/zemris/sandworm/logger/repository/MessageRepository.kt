53e68174ef623296caf9ff949965e8f1dca24f96
Petar Šegina <psegina@ymail.com>
2017-01-15 17:03:08 +0100

    Add method for fetching messages since a specific time

--- src/main/kotlin/hr/fer/zemris/sandworm/logger/repository/MessageRepository.kt
+++ src/main/kotlin/hr/fer/zemris/sandworm/logger/repository/MessageRepository.kt
@@ -2,9 +2,12 @@ package hr.fer.zemris.sandworm.logger.repository
 
 import hr.fer.zemris.sandworm.logger.model.Message
 import org.springframework.data.repository.CrudRepository
+import java.time.LocalDateTime
 
 interface MessageRepository : CrudRepository<Message, Long> {
 
     fun findByTaskId(taskId: String): List<Message>
+
+    fun findByTaskIdAndTimestampGreaterThan(taskId: String, timestamp: LocalDateTime): List<Message>
 
 }
